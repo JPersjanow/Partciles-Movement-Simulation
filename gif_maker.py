de70649fe710f528cc519357c9fd4a3275fb6df7
Jakub Persjanow <jakubpersjanow@gmail.com>
2023-06-12 00:36:36 +0200

    added gif maker

---
+++ gif_maker.py
@@ -0,0 +1,16 @@
+import imageio
+import os
+
+filenames = os.listdir("figs")
+filenames_sorted = sorted(filenames, key=lambda x: int(x.split("-")[1].split(".")[0]))
+print(filenames_sorted)
+filenamse_dir = []
+for i in filenames_sorted:
+    filenamse_dir.append("figs\\" + i)
+
+print(filenamse_dir)
+
+with imageio.get_writer("figs\\dish.gif", mode="I") as writer:
+    for filename in filenamse_dir:
+        image = imageio.imread(filename)
+        writer.append_data(image)
